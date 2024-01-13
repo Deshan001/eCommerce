a50dbe19f4b3b5cf7f6304e19134d40c2f8149ea
Deshan001 <venomassassin01@gmail.com>
2024-01-13 13:59:42 +0530

    reactvapp components pages routing

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Layout from "./componants/layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1>E-Commerce Application</h1>
+      </Layout>
+    </>
+  );
+}
+export default App;
